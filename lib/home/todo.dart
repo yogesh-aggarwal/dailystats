8d0edc96255819e722320955d8a3d9a438688ddb
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-10-22 09:36:01 +0530

    Added todo section

--- lib/home/todo.dart
+++ lib/home/todo.dart
@@ -3,8 +3,48 @@ import 'package:flutter/material.dart';
 class HomeTodoWiget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Todo"),
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Todo",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  GestureDetector(
+                    child: Text(
+                      "View All",
+                      style: TextStyle(
+                        color: Theme.of(context).accentColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return Text(index.toString());
+                },
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
