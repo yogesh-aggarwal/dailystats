733d41a7641e8ba3c5024d24526028e4b325a793
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-10-22 09:28:34 +0530

    Decreased appbar height

--- lib/home/appbar.dart
+++ lib/home/appbar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeAppBar extends PreferredSize {
-  final double height = 200;
+  final double height = 150;
 
   @override
   Size get preferredSize => Size.fromHeight(height);
